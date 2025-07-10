adea07787dcd6e27b8b44f8909f375fa85e4541d
Iwlj4s <elizavetalenn@mail.ru>
2025-07-10 10:42:15 +0300

    Models changes

--- database/models.py
+++ database/models.py
@@ -1,24 +1,32 @@
 from typing import List
-from sqlalchemy import String, Text, text, ForeignKey, DateTime, func, Column, Integer
+from sqlalchemy import String, ForeignKey, Column, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database.database import Base
 
 
-class Something(Base):
-    __tablename__ = 'something'
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, nullable=False, index=True, unique=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-
-    user = relationship("User", back_populates="something")
-
-
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False, unique=True, index=True)
-    email = Column(String, nullable=False, unique=True, index=True)
-    password = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+    password: Mapped[str] = mapped_column(String)
 
-    something = relationship("Something", back_populates="user")
+    something: Mapped[List["Something"]] = relationship(
+        "Something",
+        back_populates="user",
+        lazy="selectin"
+    )
+
+
+class Something(Base):
+    __tablename__ = 'something'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user: Mapped["User"] = relationship(
+        "User",
+        back_populates="something",
+        lazy="selectin"
+    )
